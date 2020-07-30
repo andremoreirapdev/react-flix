1c71d85d935d36d0299ab74e6c5069f50d526cbc
André P Moreira <40836023+moreiraandrep@users.noreply.github.com>
2020-07-30 00:06:04 -0300

    Update index.js
    Alteração Footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,10 +8,10 @@ function Footer() {
         <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
       </a>
       <p>
-        Orgulhosamente criado durante a
+        X-Force
         {' '}
         <a href="https://www.alura.com.br/">
-          Imersão React da Alura
+          Team - Maringá - PR
         </a>
       </p>
     </FooterBase>
